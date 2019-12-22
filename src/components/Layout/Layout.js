c23e2246b17380302c4c949d514daa311185459a
Bohdan Melvic <bohdanmelvic@gmail.com>
2019-12-22 18:29:17 +0200

    added toolbar and logo components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
 import './Layout.scss'
+import ToolBar from '../Navigation/ToolBar/ToolBar';
 
 export default function Layout(props) {
     return (
         <Auxiliary>
-            <div>
-                Toolbar, Sidebar, Backdrop
-            </div>
+            <ToolBar />
             <main className='Content'>
                 {props.children}
             </main>
---
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ToolBar.scss'
+import Logo from '../../Logo/Logo';
+
+export default function ToolBar(props) {
+    return (
+        <header className='ToolBar'>
+            <div>MENU</div>
+            <Logo />
+            <nav>
+                ....
+            </nav>
+        </header>
+    )
+}
