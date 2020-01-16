c1aab86ede21b88bce2a89c3448534976c4408e8
Bohdan Melvic <bohdanmelvic@gmail.com>
2020-01-16 13:15:26 +0200

    Fixing the Redirect to the Frontpage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.scss';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
@@ -16,21 +16,42 @@ export class App extends Component{
   }
 
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" exact component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to='/' />
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path='/orders' component={Orders} />
+          <Route path="/logout" exact component={Logout} />
+          <Route path="/auth" exact component={Auth} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to='/' />
+        </Switch>
+      );
+    }
+
     return (
       <div className="App">
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path='/orders' component={Orders} />
-            <Route path="/auth" exact component={Auth} />
-            <Route path="/logout" exact component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -38,4 +59,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -14,7 +14,7 @@ export default function SideDrawer(props) {
     return (
         <Auxiliary>
             <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={attachedClasses.join(' ')}>
+            <div className={attachedClasses.join(' ')} onClick={props.closed} >
                 <div className='SideDrawerLogo'>
                     <Logo />
                 </div>
